581e0950aed72ced3be4c53928eb01d5efa0f0ce
talupulasekhar <69070724+talupulasekhar@users.noreply.github.com>
2024-06-26 20:46:39 +0530

    Update run_code_two.py

--- me/personal/perform_calc/run_code_two.py
+++ me/personal/perform_calc/run_code_two.py
@@ -1,13 +1,14 @@
 
 import os
-from python-generic-functon-library-two-custom-code.custom.functs.math_functs import s_add_square , s_sub_square
+import python-generic-functon-library-two-custom-code as p
+#from python-generic-functon-library-two-custom-code.custom.functs.math_functs import s_add_square , s_sub_square
 
 
 
 def __main__():
     print("## from main method")
-    print(s_add_square(10,30))
-    print(s_sub_square(40,50))
+    print(p.s_add_square(10,30))
+    print(p.s_sub_square(40,50))
 
 
 if __name__ == "__main__" :

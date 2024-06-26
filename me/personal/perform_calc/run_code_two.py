1fd3538f9c3b0f4be5faa9b00cacfc3e3d60ee30
talupulasekhar <69070724+talupulasekhar@users.noreply.github.com>
2024-06-26 21:42:57 +0530

    Update run_code_two.py

--- me/personal/perform_calc/run_code_two.py
+++ me/personal/perform_calc/run_code_two.py
@@ -6,8 +6,8 @@ from custom.functs.math_functs import s_add_square , s_sub_square
 
 def __main__():
     print("## from main method")
-    print(p.s_add_square(10,30))
-    print(p.s_sub_square(40,50))
+    print(s_add_square(10,30))
+    print(s_sub_square(40,50))
 
 
 if __name__ == "__main__" :

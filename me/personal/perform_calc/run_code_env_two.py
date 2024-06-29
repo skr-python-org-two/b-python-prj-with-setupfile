8f307ef22b86404799bb5d6e4e7168bd615bb061
sekhar talupula <sekher.talupula@gmail.com>
2024-06-29 09:25:21 +0530

    updated code

---
+++ me/personal/perform_calc/run_code_env_two.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import pathlib
+import os
+
+
+def main():
+    print("### from run_code_env_two file")
+    current_dir = pathlib.Path(__file__).parent.resolve()
+    print(f" current_dir value is ::: {current_dir}")
+    PROJ_BASE_DIRECTORY = os.getenv("PROJ_BASE_DIR")
+    print(f"env value of PROJ_BASE_DIR is :::: ")
+    print(os.getenv("PROJ_BASE_DIR"))
+    print(f"   ::::::::::::::::::::::::::::::::::::::::   ")
+    load_dotenv(f"{PROJ_BASE_DIRECTORY}/.env_dev")
+    print(os.getenv("VAR_A"))
+    print(os.getenv("VAR_B"))
+    print(os.getenv("VAR_C"))
+
+
+if __name__ == "__main__":
+    main()
--- me/personal/perform_calc/run_env_vars.py
+++ me/personal/perform_calc/run_env_vars.py
@@ -1,8 +1,9 @@
 import os
 
 def __main__():
+    print("### from run_code_env_two file")
     os.environ['first_var'] = 'hello'
-    print(" ### from run_env_vars")
+
     for key , value in os.environ.items():
         print(f"{key}    {value}")
 

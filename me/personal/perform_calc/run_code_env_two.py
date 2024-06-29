db9ba8c7d5b26e9658a618003a7ff26fdd67486c
sekhar talupula <sekher.talupula@gmail.com>
2024-06-29 09:45:30 +0530

    updated code

--- me/personal/perform_calc/run_code_env_two.py
+++ me/personal/perform_calc/run_code_env_two.py
@@ -6,8 +6,9 @@ import os
 def main():
     print("### from run_code_env_two file")
     print(os.getenv("GH_BASE_DIR"))
-    BDIR = os.getenv("GH_BASE_DIR")
-    load_dotenv(f"{BDIR}/.env_dev")
+    BDIR_value = os.getenv("GH_BASE_DIR")
+    env_value = os.getenv("env")
+    load_dotenv(f"{BDIR_value}/.env_{env_value}")
     print(os.getenv("VAR_A"))
     print(os.getenv("VAR_B"))
     print(os.getenv("VAR_C"))
